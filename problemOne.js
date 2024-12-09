53755b25d84da5020b3e2afc0edadcf1698f7c09
Prince Singh <103892555+princesinghchauhan@users.noreply.github.com>
2024-12-09 17:48:14 +0530

    create problem one task

---
+++ problemOne.js
@@ -0,0 +1,29 @@
+function countCharacters(input){
+    let count = {}
+    let result = ""
+    input = input.toUpperCase()
+
+    for (let i= 0; i < input.length; i++){
+        let char = input[i]
+        if(char === ' ')continue
+
+        if(!count[char]){
+            count[char]=1
+            result += char + "-"
+        }else{
+            count[char]++
+        }
+    }
+
+    let output =""
+    for(let i =0; i<result.length; i+=2){
+        let char = result[i]
+        if(char){
+            output += `${char}-${count[char]}\n`
+
+        }
+    }
+    return output.trim()
+}
+
+console.log(countCharacters("Amolya Sharma"))
